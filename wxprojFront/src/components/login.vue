a390a92f712c329107cb69e7d5a6251c724edf29
hengqu4 <529914423@qq.com>
2020-12-07 21:55:21 +0800

    tarbar

---
+++ wxprojFront/src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <button class="login-btn" open-type="getUserInfo" @getuserinfo="getUserInfo">
+        授权登录
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    mounted (){
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ wxprojFront/src/pages/report/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button>login</button>
+  </div>
+</template>
+
+<script>
+import login from "../../components/login"
+
+export default {
+  data () {
+    return {
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
